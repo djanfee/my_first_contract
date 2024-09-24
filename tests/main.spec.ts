ad4ebdf653775df4d0bb01546ddd025ca3862e23
djanfee <djanfee@gmail.com>
2024-09-24 10:53:38 +0800

    带有计数器的合同的逻辑

--- tests/main.spec.ts
+++ tests/main.spec.ts
@@ -7,30 +7,40 @@ import "@ton/test-utils";
 
 describe("main.fc contract tests", () => {
 
-    it("our first test", async () => {
-        const blockchain = await Blockchain.create();
-        const codeCell = Cell.fromBoc(Buffer.from(hex, "hex"))[0];
-    
-        const myContract = blockchain.openContract(
-          await MainContract.createFromConfig({}, codeCell)
-        );
-    
-        const senderWallet = await blockchain.treasury("sender");
-    
-        const sentMessageResult = await myContract.sendInternalMessage(
-          senderWallet.getSender(),
-          toNano("0.05")
-        );
-    
-        expect(sentMessageResult.transactions).toHaveTransaction({
-          from: senderWallet.address,
-          to: myContract.address,
-          success: true,
-        });
-    
-        const data = await myContract.getData();
-    
-        expect(data.recent_sender.toString()).toBe(senderWallet.address.toString());
+  it("our first test", async () => {
+    const blockchain = await Blockchain.create();
+    const codeCell = Cell.fromBoc(Buffer.from(hex, "hex"))[0];
+
+    const initAddress = await blockchain.treasury("initAddress");
+
+    const myContract = blockchain.openContract(
+      await MainContract.createFromConfig(
+        {
+          number: 0,
+          address: initAddress.address,
+        },
+        codeCell
+      )
+    );
+
+    const senderWallet = await blockchain.treasury("sender");
+
+    const sentMessageResult = await myContract.sendIncrement(
+      senderWallet.getSender(),
+      toNano("0.05"),
+      1
+    );
+
+    expect(sentMessageResult.transactions).toHaveTransaction({
+      from: senderWallet.address,
+      to: myContract.address,
+      success: true,
     });
+
+    const data = await myContract.getData();
+
+    expect(data.recent_sender.toString()).toBe(senderWallet.address.toString());
+    expect(data.number).toEqual(1);
+  });
 
 });
